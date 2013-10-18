25ad87f836064706bdbdebc0667f274b78ce555d
Kevin Pors <krpors@gmail.com>
2013-10-18 17:29:28 +0200

    woot, can create a mosaic thingy now

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/jpeg"
+	"log"
 	"os"
 )
 
-func calcAvg(img image.Image) color.Color {
-	fmt.Println(img.Bounds())
+// Calculates the average color used in the specified rectangle in the image.
+// Will return a color.RGBA()
+func calcAvg(img image.Image, rect image.Rectangle) color.Color {
 	var r, g, b, iteration uint32
-	for x := 0; x < img.Bounds().Max.X; x++ {
-		for y := 0; y < img.Bounds().Max.Y; y++ {
+	for x := rect.Min.X; x < rect.Max.X; x++ {
+		for y := rect.Min.Y; y < rect.Max.Y; y++ {
 			// get colors at this pixel. Docs say the values returned by RGBA are
 			// alpha-premultiplied values, where each value ranges between [0-0xFFFF].
 			// We'll remove the alpha part by byteshifting if 8 places to the right.
@@ -27,30 +28,60 @@ func calcAvg(img image.Image) color.Color {
 	return color.RGBA{uint8(r / iteration), uint8(g / iteration), uint8(b / iteration), 0xff}
 }
 
-func main() {
-	of, err := os.Create("lol.jpg")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+// Fills a rectangle in the specified rgba with the given color
+func fillRect(rgba *image.RGBA, rect image.Rectangle, color color.Color) {
+	for x := rect.Min.X; x <= rect.Max.X; x++ {
+		for y := rect.Min.Y; y <= rect.Max.Y; y++ {
+			rgba.Set(x, y, color)
+		}
 	}
+}
 
-	_ = color.White
-	myimg := image.NewRGBA(image.Rect(0, 0, 640, 480))
-	for x := 0; x < 100; x++ {
-		for y := 0; y < 100; y++ {
-			myimg.Set(x, y, color.RGBA{0, 55, 210, 255})
+func divide(img image.Image, rwidth, rheight int) {
+	b := img.Bounds()
+
+
+	// create image to write to:
+	rgba := image.NewRGBA(b)
+
+	for x := 0; x < b.Max.X; x += rwidth {
+		for y := 0; y < b.Max.Y; y += rheight {
+			x2 := x+rwidth
+			y2 := y+rheight
+			// stay within bounds of the original image here:
+			if x2 >= b.Max.X {
+				x2 = b.Max.X
+			}
+			if y2 >= b.Max.Y {
+				y2 = b.Max.Y
+			}
+			bounds := image.Rect(x, y, x2, y2)
+			avgcolor := calcAvg(img, bounds)
+			fillRect(rgba, bounds, avgcolor)
 		}
 	}
 
-	subimg := myimg.SubImage(image.Rect(0, 0, 100, 100))
-	avgcolor := calcAvg(subimg)
-
-	err = jpeg.Encode(of, myimg, &jpeg.Options{90})
+	of, err := os.Create("/home/dump/LOOOOL.jpg")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+		log.Fatal(err)
 	}
 
-	fmt.Println("Average color: ", avgcolor)
-	os.Exit(0)
+	newImage := rgba.SubImage(b)
+	err = jpeg.Encode(of, newImage, &jpeg.Options{80})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	f, err := os.Open("img.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	img, err := jpeg.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	divide(img, 610, 60)
 }
