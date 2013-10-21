1625ac3f92ae499a08fb33def889563b31e14dfe
Kevin Pors <krpors@gmail.com>
2013-10-21 15:45:26 +0200

    Added downscaling.
    This is done using area-averaging, and I feel pretty proud of finding
    out this algorithm without consulting web-sites for existing algorithms
    :)

--- main.go
+++ main.go
@@ -9,26 +9,28 @@ import (
 )
 
 // Calculates the average color used in the specified rectangle in the image.
-// Will return a color.RGBA()
 func calcAvg(img image.Image, rect image.Rectangle) color.Color {
 	var r, g, b, iteration uint32
 	for x := rect.Min.X; x < rect.Max.X; x++ {
 		for y := rect.Min.Y; y < rect.Max.Y; y++ {
-			// get colors at this pixel. Docs say the values returned by RGBA are
-			// alpha-premultiplied values, where each value ranges between [0-0xFFFF].
-			// We'll remove the alpha part by byteshifting if 8 places to the right.
 			cr, cg, cb, _ := img.At(x, y).RGBA()
-			r += cr >> 8
-			g += cg >> 8
-			b += cb >> 8
+			r += cr
+			g += cg
+			b += cb
 			iteration++
 		}
 	}
 
-	return color.RGBA{uint8(r / iteration), uint8(g / iteration), uint8(b / iteration), 0xff}
+	// We'll divide the r,g,b parts by the amount of iterations, then strip off the
+	// alpha part so we can return it as an RGBA
+	avgRed := uint8((r / iteration) >> 8)
+	avgGreen := uint8((g / iteration) >> 8)
+	avgBlue := uint8((b / iteration) >> 8)
+
+	return color.NRGBA{avgRed, avgGreen, avgBlue, 0xFF}
 }
 
-// Fills a rectangle in the specified rgba with the given color
+// Fills a rectangle in the specified rgba with the given color.
 func fillRect(rgba *image.RGBA, rect image.Rectangle, color color.Color) {
 	for x := rect.Min.X; x <= rect.Max.X; x++ {
 		for y := rect.Min.Y; y <= rect.Max.Y; y++ {
@@ -37,17 +39,45 @@ func fillRect(rgba *image.RGBA, rect image.Rectangle, color color.Color) {
 	}
 }
 
-func pixelize(img image.Image, rwidth, rheight int) {
-	b := img.Bounds()
+// This function downscales an image by the specified ratio, using area-averaging.
+// For instance, specifying ratio of `2' will downscale the image to half the size
+// of the image (width and height will both be divided by 2).
+func downscaleRatio(src image.Image, ratio int) image.Image {
+	newrect := image.Rect(0, 0, src.Bounds().Max.X/ratio, src.Bounds().Max.Y/ratio)
+	rgba := image.NewRGBA(newrect)
 
+	for x := 0; x <= src.Bounds().Max.X; x += ratio {
+		for y := 0; y <= src.Bounds().Max.Y; y += ratio {
+			sgm := image.Rect(x, y, x+ratio, y+ratio)
+			color := calcAvg(src, sgm)
+			rgba.Set(x/ratio, y/ratio, color)
+
+		}
+	}
+
+	return rgba.SubImage(rgba.Bounds())
+}
+
+// Downscales an image to the specified width, maintaining aspect ratio. This will
+// call downscaleRatio(), where the specified ratio will be the source's image width
+// divided by the specified target width. For example, if the source image width is
+// 4000 pixels, the target width is 2000, the aspect ratio will be 4000/2000 = 2.
+func downscaleWidth(src image.Image, width int) image.Image {
+	return downscaleRatio(src, src.Bounds().Max.X/width)
+}
+
+// 'Pixelizes' the given image, with each 'pixel' the width and height of the given
+// parameters. Returns a new image as a result.
+func pixelize(img image.Image, rwidth, rheight int) image.Image {
+	b := img.Bounds()
 
 	// create image to write to:
 	rgba := image.NewRGBA(b)
 
 	for x := 0; x < b.Max.X; x += rwidth {
 		for y := 0; y < b.Max.Y; y += rheight {
-			x2 := x+rwidth
-			y2 := y+rheight
+			x2 := x + rwidth
+			y2 := y + rheight
 			// stay within bounds of the original image here:
 			if x2 >= b.Max.X {
 				x2 = b.Max.X
@@ -61,16 +91,8 @@ func pixelize(img image.Image, rwidth, rheight int) {
 		}
 	}
 
-	of, err := os.Create("/home/dump/LOOOOL.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	newImage := rgba.SubImage(b)
-	err = jpeg.Encode(of, newImage, &jpeg.Options{80})
-	if err != nil {
-		log.Fatal(err)
-	}
+	return newImage
 }
 
 func main() {
@@ -83,5 +105,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	pixelize(img, 50, 50)
+	dsimage := downscaleRatio(img, 3)
+
+	of, err := os.Create("/home/dump/lol2.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jpeg.Encode(of, dsimage, &jpeg.Options{jpeg.DefaultQuality})
+
 }
